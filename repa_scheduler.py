e2edee7f5f9aaf357df48adfd67795429f386f0b
couozu <couozu@gmail.com>
2022-07-02 07:54:08 +0100

    sending polls functions

--- repa_scheduler.py
+++ repa_scheduler.py
@@ -2,6 +2,9 @@
 
 import requests
 import json
+import traceback
+import datetime
+import sys
 
 with open('repa_scheduler_api_key') as f:
     API_KEY = f.read().strip()
@@ -9,18 +12,55 @@ with open('repa_scheduler_api_key') as f:
 API_PREFIX = 'https://api.telegram.org/bot' + API_KEY + '/'
 
 guides_group = -1001786539369
+hosts_group = -1001658680990
+test_group = -795223636
+oncall = 174125991
 
-r = requests.get(API_PREFIX + 'sendMessage', json = {
-'chat_id': guides_group, 'text': 'test'})
-
-print('here')
-
+'''
+r = requests.get(API_PREFIX + 'getUpdates')
 print(json.dumps(r.json()))
+sys.exit(0)
+'''
 
-print('here')
+weekdays = {
+ 0: 'Mon',
+ 1: 'Tue',
+ 2: 'Wed',
+ 3: 'Thu',
+ 4: 'Fri',
+ 5: 'Sat',
+ 6: 'Sun'
+} 
 
-print(r.request.headers)
-print(r.request.url)
-print(r.request.body)
+day = datetime.datetime.utcnow()
 
-print(r.json())
+while weekdays[day.weekday()] != 'Sun':
+  day = day + datetime.timedelta(days=1)
+
+options = []
+
+for _ in range(7):
+  day = day + datetime.timedelta(days=1)
+  options.append(day.strftime('%A %d/%b')) 
+
+options.append('Show results')
+
+def sendWeeklyPoll(group, question):
+  r = requests.get(API_PREFIX + 'sendPoll', json = {
+    'chat_id': group, 
+    'question': question,
+    'options': options,
+    'is_anonymous': False,
+    'allows_multiple_answers': True
+  })
+  r.raise_for_status()
+
+try:
+  sendWeeklyPoll(hosts_group, 'Planning next week! Please choose dates you can host!')
+  sendWeeklyPoll(guides_group, 'Planning next week! Please choose the dates you can guide!')
+  res = 'Success!'
+except:
+  res =  f'Failure! {traceback.format_exc()}'
+
+requests.get(API_PREFIX + 'sendMessage', json = {'chat_id': oncall, 'text': res})
+
